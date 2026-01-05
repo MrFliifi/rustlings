882a061018f82a3681cf51bfa29f08ec98c5d76f
FehlBehr <felix.behrens@cgi.com>
2026-01-05 18:31:55 +0100

    Initial commit

---
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let cat: (&str, f64) = ("Furry McFurson", 3.5);
+
+    // TODO: Destructure the `cat` tuple in one statement so that the println works.
+    // let /* your pattern here */ = cat;
+    let name: &str = cat.0; let age: f64 = cat.1;
+    println!("{name} is {age} years old");
+}
