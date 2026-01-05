05d8dae6be9150a2176caba42cd22126701657e6
FehlBehr <felix.behrens@cgi.com>
2026-01-05 19:38:23 +0100

    finished structs

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -1,4 +1,4 @@
-// Structs contain data, but can also have logic. In this exercise, we have
+ // Structs contain data, but can also have logic. In this exercise, we have
 // defined the `Package` struct, and we want to test some logic attached to it.
 
 #[derive(Debug)]
@@ -24,14 +24,25 @@ impl Package {
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn is_international(&self) {
+    fn is_international(&self) -> bool {
         // TODO: Read the tests that use this method to find out when a package
         // is considered international.
+        let is_international: bool;
+
+        if self.sender_country != self.recipient_country {
+            is_international = true;
+        } else {
+            is_international = false;
+        }
+
+        return  is_international;
+
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn get_fees(&self, cents_per_gram: u32) {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         // TODO: Calculate the package's fees.
+        cents_per_gram * self.weight_in_grams
     }
 }
 
