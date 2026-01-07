cfcc3afeb07dded0a8e28c86b8a95acd80f721ad
FehlBehr <felix.behrens@cgi.com>
2026-01-07 20:12:49 +0100

    finished strings

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -1,6 +1,9 @@
 // TODO: Fix the compiler error without changing the function signature.
 fn current_favorite_color() -> String {
-    "blue"
+    let mut s: String = String::new();
+    let data = "blue";
+    s = data.to_string();
+    return s;
 }
 
 fn main() {
